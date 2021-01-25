0a72040d173553e66441451f3dd763896ffc35b4
Eduardo Guereque <edwardgnt@gmail.com>
2021-01-25 15:15:01 -0800

    Adding components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click');
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
@@ -14,7 +20,13 @@ Header.defaultProps = {
 }
 
 Header.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
